1b22db56789425b4716b1d262beb84a80f698277
Pranshu Dobhal <pranshu.dobhal@gmail.com>
2021-01-01 23:23:18 +0530

    refactor: removed unnecessary code and comments

--- app.js
+++ app.js
@@ -1,38 +1,46 @@
-//Take user's complete birthday
+//take user's complete birthday
 var fullBirthday = document.getElementById('birthday');
 
-//Take user's lucky number
+//take user's lucky number
 var luckyNumber = document.getElementById('lucky-number');
 
-//check if sum  of digits is of birthday is divisible by lucky number
+//click function to check if birthday is lucky or not
 document.getElementById('submit').addEventListener('click', calcLucky);
 
-//n.toString().split('').map(Number);
-
 function calcLucky() {
+  //take value of birthday and display it
   var birthday = fullBirthday.value;
+  document.getElementById('demo').innerText = birthday;
+
+  //split the birthday into array of digits
   birthday = birthday.split('');
 
+  //convert array into string and replace - and , with empty space
   var newBirth = birthday.toString().replaceAll('-', '');
   newBirth = newBirth.replaceAll(',', '');
 
+  //convert digits to integer and calculate sum
   var sumOfBirthday = 0;
   for (var i = 0; i < newBirth.length; i++) {
     sumOfBirthday += parseInt(newBirth[i]);
   }
 
+  //display sum of digits of birthday
+  document.getElementById('demo2').innerText = sumOfBirthday;
+
+  //take lucky number and convert to integer
   var number = parseInt(luckyNumber.value);
+
+  //display lucky number
+  document.getElementById('luck').innerText = number;
 
   var lucky = "I'm sorry but Your birthday is not lucky :(";
 
+  //check if sum of digits of birthday is lucky or not
   if (sumOfBirthday % number === 0) {
     lucky = 'Yayy!! Your birthday is lucky for you!!! :D';
   }
 
-  //   document.getElementById('demo').innerText = birthday;
-  document.getElementById('demo2').innerText = sumOfBirthday;
-  document.getElementById('luck').innerText = number;
-
-  //display lucky or not
+  //display if lucky or not
   document.getElementById('lucky-display').innerText = lucky;
 }
